ac28e17c4dfa5e0dabab624823ab6abb1eb6d970
brooke-delgado <83773208+brooke-delgado@users.noreply.github.com>
2021-08-28 07:44:57 -0500

    added table to hold time created save button

--- assests/script.js
+++ assests/script.js
@@ -1,71 +1,71 @@
+//Header Date
 var now = moment().format("dddd, MMMM Do YYYY");
 console.log(now)
 document.getElementById("currentDay").textContent = now;
-    
+
     var DayTimes = [
         {
 
     id: "0",
     hour: "09",
     time: "09",
-    meridiem: "am",
-    reminder: ""
+    event: "am",
+    left: ""
 },
 {
     id: "1",
     hour: "10",
     time: "10",
-    meridiem: "am",
-    reminder: ""
+    event: "am",
+    left: ""
 },
 {
     id: "2",
     hour: "11",
     time: "11",
-    meridiem: "am",
-    reminder: ""
+    event: "am",
+    left: ""
 },
 {
     id: "3",
     hour: "12",
     time: "12",
-    meridiem: "pm",
-    reminder: ""
+    event: "pm",
+    left: ""
 },
 {
     id: "4",
     hour: "01",
     time: "13",
-    meridiem: "pm",
-    reminder: ""
+    event: "pm",
+    left: ""
 },
 {
     id: "5",
     hour: "02",
     time: "14",
-    meridiem: "pm",
-    reminder: ""
+    event: "pm",
+    left: ""
 },
 {
     id: "6",
     hour: "03",
     time: "15",
-    meridiem: "pm",
-    reminder: ""
+    event: "pm",
+    left: ""
 },
 {
     id: "7",
     hour: "04",
     time: "16",
-    meridiem: "pm",
-    reminder: ""
+    event: "pm",
+    left: ""
 },
 {
     id: "8",
     hour: "05",
     time: "17",
-    meridiem: "pm",
-    reminder: ""
+    event: "pm",
+    left: ""
 },
-
 ]
