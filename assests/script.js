6e5f6b01b99aca4527bb0ecc2812b50a4a4b6a96
brooke-delgado <83773208+brooke-delgado@users.noreply.github.com>
2021-08-28 20:22:16 -0500

    changed tables to div and rows to make the css match, added some styling, relinked js

--- assests/script.js
+++ assests/script.js
@@ -3,69 +3,7 @@ var now = moment().format("dddd, MMMM Do YYYY");
 console.log(now)
 document.getElementById("currentDay").textContent = now;
 
-    var DayTimes = [
-        {
 
-    id: "0",
-    hour: "09",
-    time: "09",
-    event: "am",
-    left: ""
-},
-{
-    id: "1",
-    hour: "10",
-    time: "10",
-    event: "am",
-    left: ""
-},
-{
-    id: "2",
-    hour: "11",
-    time: "11",
-    event: "am",
-    left: ""
-},
-{
-    id: "3",
-    hour: "12",
-    time: "12",
-    event: "pm",
-    left: ""
-},
-{
-    id: "4",
-    hour: "01",
-    time: "13",
-    event: "pm",
-    left: ""
-},
-{
-    id: "5",
-    hour: "02",
-    time: "14",
-    event: "pm",
-    left: ""
-},
-{
-    id: "6",
-    hour: "03",
-    time: "15",
-    event: "pm",
-    left: ""
-},
-{
-    id: "7",
-    hour: "04",
-    time: "16",
-    event: "pm",
-    left: ""
-},
-{
-    id: "8",
-    hour: "05",
-    time: "17",
-    event: "pm",
-    left: ""
-},
-]
+
+    
+   
